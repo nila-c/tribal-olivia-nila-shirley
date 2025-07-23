ef95e457249bb73d6c135f732de23112c7dcfedc
Nila Cibu <nilacibu@gmail.com>
2025-07-23 03:32:09 -0700

    simple regex for extracting sensor and site name

--- cleaning/reading_data.R
+++ cleaning/reading_data.R
@@ -7,3 +7,13 @@ here::i_am("cleaning/reading_data.R")
 # loading data ----------------------
 sklallam_raw <- read_csv(here("data_for_proj3", "dungeness1.csv"), na = "", skip = 1)
 hoh_wqts <- readRDS(here("data_for_proj3", "hoh_wqts.rds"))
+
+# creating clean dataset ------------
+
+## extracting names -----------------
+
+orig_name <- "Copy of HOBO-temp-DO-Model-U26-001- Site-Dungeness 1"
+
+sensor <- str_extract(string = colnames(sklallam_raw)[3], pattern = "\\d+") %>%
+  as.numeric()
+site <- str_extract(orig_name, pattern = "(?<=Site-).*$")

1368a06e4d3bc96e55eb649c84d8d17b318fa209
Nila Cibu <nilacibu@gmail.com>
2025-08-05 20:36:16 +0100

    code for cropping data

---
+++ cleaning/cropping.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(dataQCtools)
+library(here)
+library(data.table)
+library(readxl)
+
+here::i_am("cleaning/cropping.R")
+
+# loading data -------------------
+
+# location data
+path <- here("data_for_proj3", "jst_data_ldr_times", "extdata", "data", "2015_summer")
+data_paths_orig <- list.files(path = path, pattern = "\\.csv$") %>% as.vector()
+data_paths <- paste(path, "/", data_paths_orig, sep = "")
+loc_data <- sapply(data_paths, fread, skip = 1, blank.lines.skip = TRUE)
+names(loc_data) <- data_paths_orig
+
+# ldr times data
+ldr_name <- list.files(path = path, pattern="\\.xlsx$") %>% as.vector()
+ldr_path <- paste(path, "/", ldr_name, sep = "")
+ldr_times <- read_excel(ldr_path)
+
+# cropping ----------------------
+data_paths_nospace <- sapply(data_paths, str_replace_all, pattern = " ", replacement = "")
+file.rename(data_paths, data_paths_nospace)
+paste0(path, "/") %>%
+  crop_raw_data(ldrtimes_fn = ldr_name, cropped_loc = here("data_for_proj3"))
